409d85d43b017609f0f87bbc0f864b0cc57868d6
Kene <kayyungx@gmail.com>
2022-10-26 11:30:58 +0100

    Add calculator functionality

--- src/components/calculator/Calculate.js
+++ src/components/calculator/Calculate.js
@@ -1,49 +1,148 @@
 import { Component } from 'react';
 import classes from './Calculate.module.css';
+import calculate from '../../logic/calculate.js';
 
 class Calculate extends Component {
   constructor(props) {
     super(props);
     this.props = props;
+    this.state = {};
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  clickHandler(e) {
+    this.setState((state) => calculate(state, e.target.textContent));
   }
 
   render() {
+    const { next, total } = this.state;
     return (
       <div className={this.props.className}>
         <div className={classes.calculator}>
-          <div className={classes.row1}>0</div>
+          <div className={classes.row1}>{next || total || 0}</div>
           <div className={classes.row}>
-            <div className={`${classes.btn} ${classes.btn_grey}`}>AC</div>
-            <div className={`${classes.btn} ${classes.btn_grey}`}>+/-</div>
-            <div className={`${classes.btn} ${classes.btn_grey}`}>%</div>
-            <div className={`${classes.btn} ${classes.btn_orange}`}>+</div>
+            <div
+              className={`${classes.btn} ${classes.btn_grey}`}
+              onClick={this.clickHandler}
+            >
+              AC
+            </div>
+            <div
+              className={`${classes.btn} ${classes.btn_grey}`}
+              onClick={this.clickHandler}
+            >
+              +/-
+            </div>
+            <div
+              className={`${classes.btn} ${classes.btn_grey}`}
+              onClick={this.clickHandler}
+            >
+              %
+            </div>
+            <div
+              className={`${classes.btn} ${classes.btn_orange}`}
+              onClick={this.clickHandler}
+            >
+              +
+            </div>
           </div>
           <div className={classes.row}>
-            <div className={`${classes.btn} ${classes.btn_grey}`}>7</div>
-            <div className={`${classes.btn} ${classes.btn_grey}`}>8</div>
-            <div className={`${classes.btn} ${classes.btn_grey}`}>9</div>
-            <div className={`${classes.btn} ${classes.btn_orange}`}>x</div>
+            <div
+              className={`${classes.btn} ${classes.btn_grey}`}
+              onClick={this.clickHandler}
+            >
+              7
+            </div>
+            <div
+              className={`${classes.btn} ${classes.btn_grey}`}
+              onClick={this.clickHandler}
+            >
+              8
+            </div>
+            <div
+              className={`${classes.btn} ${classes.btn_grey}`}
+              onClick={this.clickHandler}
+            >
+              9
+            </div>
+            <div
+              className={`${classes.btn} ${classes.btn_orange}`}
+              onClick={this.clickHandler}
+            >
+              x
+            </div>
           </div>
           <div className={classes.row}>
-            <div className={`${classes.btn} ${classes.btn_grey}`}>4</div>
-            <div className={`${classes.btn} ${classes.btn_grey}`}>5</div>
-            <div className={`${classes.btn} ${classes.btn_grey}`}>6</div>
-            <div className={`${classes.btn} ${classes.btn_orange}`}>-</div>
+            <div
+              className={`${classes.btn} ${classes.btn_grey}`}
+              onClick={this.clickHandler}
+            >
+              4
+            </div>
+            <div
+              className={`${classes.btn} ${classes.btn_grey}`}
+              onClick={this.clickHandler}
+            >
+              5
+            </div>
+            <div
+              className={`${classes.btn} ${classes.btn_grey}`}
+              onClick={this.clickHandler}
+            >
+              6
+            </div>
+            <div
+              className={`${classes.btn} ${classes.btn_orange}`}
+              onClick={this.clickHandler}
+            >
+              -
+            </div>
           </div>
           <div className={classes.row}>
-            <div className={`${classes.btn} ${classes.btn_grey}`}>1</div>
-            <div className={`${classes.btn} ${classes.btn_grey}`}>2</div>
-            <div className={`${classes.btn} ${classes.btn_grey}`}>3</div>
-            <div className={`${classes.btn} ${classes.btn_orange}`}>÷</div>
+            <div
+              className={`${classes.btn} ${classes.btn_grey}`}
+              onClick={this.clickHandler}
+            >
+              1
+            </div>
+            <div
+              className={`${classes.btn} ${classes.btn_grey}`}
+              onClick={this.clickHandler}
+            >
+              2
+            </div>
+            <div
+              className={`${classes.btn} ${classes.btn_grey}`}
+              onClick={this.clickHandler}
+            >
+              3
+            </div>
+            <div
+              className={`${classes.btn} ${classes.btn_orange}`}
+              onClick={this.clickHandler}
+            >
+              ÷
+            </div>
           </div>
           <div className={classes.row}>
             <div
               className={`${classes.btn} ${classes.btn_0} ${classes.btn_grey}`}
+              onClick={this.clickHandler}
             >
               0
             </div>
-            <div className={`${classes.btn} ${classes.btn_grey}`}>.</div>
-            <div className={`${classes.btn} ${classes.btn_orange}`}>=</div>
+            <div
+              className={`${classes.btn} ${classes.btn_grey}`}
+              onClick={this.clickHandler}
+            >
+              .
+            </div>
+            <div
+              className={`${classes.btn} ${classes.btn_orange}`}
+              onClick={this.clickHandler}
+            >
+              =
+            </div>
           </div>
         </div>
       </div>
