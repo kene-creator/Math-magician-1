b4a195afa1e0dd130e79a1c2a20e26e2980e953b
Kene <kayyungx@gmail.com>
2022-11-01 12:08:01 +0100

    Add navigation bar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@
 import Calculate from './components/calculator/Calculate.js';
 import classes from './components/calculator/Calculate.module.css';
 import { Component } from 'react';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Naviagtion from './components/UI/Navigation.js';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Calculate className={classes.containers} />
-      </div>
+      <React.Fragment>
+        <Naviagtion />
+        <Routes>
+          <Route
+            path="/"
+            element={<Calculate className={classes.containers} />}
+          ></Route>
+        </Routes>
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/UI/Navigation.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import classes from './Navigation.module.css';
+
+/* eslint-disable */
+const Naviagtion = () => {
+  return (
+    <nav className={classes.nav}>
+      <ul className={classes.link_items}>
+        <li>
+          <Link to="/" className={classes.link}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className={classes.link}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/qoute" className={classes.link}>
+            Qoute
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Naviagtion;
